43724b2a0e36866b129b1a36b61340c957d6b1a3
Shinter <Shinter@SHIN-PC>
2021-07-31 22:04:42 +0400

    Add project files.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using MelonLoader;
+using NiceHotkeys;
+
+[assembly: MelonInfo(typeof(NiceHotkeyMod), "Nice Hotkeys", "1.0.0", "Shin")]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
---
+++ NiceHotkeyMod.cs
@@ -0,0 +1,44 @@
+﻿using ABI_RC.Core;
+using ABI_RC.Core.EventSystem;
+using ABI_RC.Core.Player;
+using UnityEngine;
+using MelonLoader;
+
+namespace NiceHotkeys
+{
+    public sealed class NiceHotkeyMod : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.K) && Input.GetKey(KeyCode.LeftControl))
+            {
+                ResetAvatar();
+            }
+            if (Input.GetKeyDown(KeyCode.R) && Input.GetKey(KeyCode.LeftControl))
+            {
+                Respawn();
+            }
+            if (Input.GetKeyDown(KeyCode.Z) && Input.GetKey(KeyCode.LeftControl))
+            {
+                RemoveAllAvatars();
+            }
+        }
+        public static void ResetAvatar()
+        {
+            AssetManagement.Instance.LoadLocalAvatar("17c267db-18c4-4900-bb73-ad323f082640"); // changes your avatar to space robot kyle
+            MelonLogger.Msg("Avatar was reset."); // this just logs to melon logger
+        }
+        public static void Respawn()
+        {
+            RootLogic.Instance.Respawn();
+            MelonLogger.Msg("Respawned.");
+        }
+
+        public static void RemoveAllAvatars()
+        {
+            CVRPlayerManager.Instance.ClearPlayerAvatars();
+            MelonLogger.Msg("All avatars were removed.");
+        }
+
+    }
+}
